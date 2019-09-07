f4c82cdc303cb4daa9b90b230c8de413c26ecfac
shashodakalindu <shashoda317@gmail.com>
2019-09-07 10:31:25 +0530

    changes in interfaces

--- app/src/main/java/com/example/makereservation/cnt.java
+++ app/src/main/java/com/example/makereservation/cnt.java
@@ -1,4 +1,49 @@
 package com.example.makereservation;
 
 public class cnt {
+
+    private String contactName;
+    private String contactEmail;
+    private String contactPhone;
+    private String contactMessage;
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactMessage() {
+        return contactMessage;
+    }
+
+    public void setContactMessage(String contactMessage) {
+        this.contactMessage = contactMessage;
+    }
+
+
+
+
+    public cnt() {
+
+
+    }
 }
--- app/src/main/java/com/example/makereservation/contactus2.java
+++ app/src/main/java/com/example/makereservation/contactus2.java
@@ -3,9 +3,18 @@ package com.example.makereservation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class contactus2 extends AppCompatActivity {
 
@@ -14,10 +23,10 @@ public class contactus2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contactus2);
 
-        EditText nt1 = findViewById(R.id.contactName);
-        EditText nt2 = findViewById(R.id.editText0);
-        EditText nt3 = findViewById(R.id.editText10);
-        EditText nt4 = findViewById(R.id.editText11);
+       final EditText nt1 = findViewById(R.id.contactName);
+       final EditText nt2 = findViewById(R.id.editText0);
+       final EditText nt3 = findViewById(R.id.editText10);
+       final EditText nt4 = findViewById(R.id.editText11);
 
         Intent intent2 = getIntent();
 
@@ -31,10 +40,46 @@ public class contactus2 extends AppCompatActivity {
         nt3.setText(s3);
         nt4.setText(s4);
 
+        Button viw = findViewById(R.id.button14);
+        final EditText t1 = findViewById(R.id.txt);
+
+        viw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("cnt").child(t1.getText().toString());
+                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()){
+                            nt1.setText(dataSnapshot.child("contactName").getValue().toString());
+                            nt2.setText(dataSnapshot.child("contactEmail").getValue().toString());
+                            nt3.setText(dataSnapshot.child("contactPhone").getValue().toString());
+                            nt4.setText(dataSnapshot.child("contactMessage").getValue().toString());
+                            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No source to display",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                } );{
+
+                };
+
+            }
+        }); {
+
     }
 
-    public void onUpdateButtonPushed(View view){
-        Intent update = new Intent(contactus2.this, contactus3.class);
-        startActivity(update);
+ //    public void onUpdateButtonPush(View view ){
+   //     Intent update = new Intent(contactus2.this, contactus3.class);
+    //    startActivity(update);
+    //}
+
     }
 }
