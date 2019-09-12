5f0edf6d36e48cdf54dc2d8f8e1ec56b1fbbee6a
janitha_Akalanka <janithaakalanka45@gmail.com>
2019-09-12 21:55:04 +0530

    Create new home page

--- app/src/main/java/com/example/makereservation/Home.java
+++ app/src/main/java/com/example/makereservation/Home.java
@@ -1,62 +1,57 @@
 package com.example.makereservation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Home extends AppCompatActivity {
-    Button Reservation;
-    Button pay1, contactBtn;
-    Button payprocces;
+
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        Reservation= (Button) findViewById(R.id.Reservation);
-
-
-        Reservation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), makereservation1.class);
-                startActivity(i);
-            }
-
-        });
-
-
-       payprocces = findViewById(R.id.bookNowBtn7);
-
-        payprocces.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(),paymentnew.class);
-                startActivity(intent);
-
-           }
-        });
-
-
-    }
-    //button on click
-
-    public void clickRooms(View v){
-
-        Intent click =new Intent(this, checkavailability1.class);
-        startActivity(click);
     }
 
-    public void contactUsClick(View view) {
-        Intent click =new Intent(this, contactus1.class);
-        startActivity(click);
+        public void Resavation(View view)
+        {
+            Intent i1 = new Intent (Home.this,makereservation.class);
+            startActivity(i1);
+        }
+
+     //   public void Rates(View view)
+     //   {
+      //      Intent i1 = new Intent (Home.this,.class);
+     //       startActivity(i1);
+     //   }
+
+
+     //   public void room(View view)
+     //   {
+     //       Intent i1 = new Intent (Home.this,.class);
+     //       startActivity(i1);
+    //}
+
+        public void contact(View view)
+        {
+            Intent i1 = new Intent (Home.this,contactus1.class);
+            startActivity(i1);
+        }
+
+        public void Payment(View view)
+        {
+            Intent i1 = new Intent (Home.this,paymentnew.class);
+            startActivity(i1);
+        }
+
+     //   public void facilities(View view){
+     //       Intent intent = new Intent(Home.this,.class);
+     //       startActivity(intent);
+     //   }
 
     }
 
-
-}
--- app/src/main/java/com/example/makereservation/paymentnew.java
+++ app/src/main/java/com/example/makereservation/paymentnew.java
@@ -2,9 +2,17 @@ package com.example.makereservation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class paymentnew extends AppCompatActivity {
@@ -27,4 +35,7 @@ public class paymentnew extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
