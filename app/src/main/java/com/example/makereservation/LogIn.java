1d5cd854343d9e70cf4a6a090148035c6521319b
Sapthaka Godage <sapthakagodage31@gmail.com>
2019-09-13 11:30:36 -0700

    login validation

--- app/src/main/java/com/example/makereservation/LogIn.java
+++ app/src/main/java/com/example/makereservation/LogIn.java
@@ -3,33 +3,58 @@ package com.example.makereservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
+
+import static android.text.TextUtils.isEmpty;
 
 public class LogIn extends AppCompatActivity {
-    Button userloginbtn;
+    private EditText passwordtxt;
+    private EditText usernametxt;
     Button btnlogin;
     Button adminloginbtn;
+    private int counter = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getSupportActionBar().hide();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
 
-        userloginbtn = (Button) findViewById(R.id.userloginbtn);
-        adminloginbtn=(Button)findViewById(R.id.adminloginbtn);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        adminloginbtn = (Button) findViewById(R.id.adminloginbtn);
+        passwordtxt = (EditText) findViewById(R.id.passwordtxt);
+        usernametxt = (EditText) findViewById(R.id.usernametxt);
 
-        userloginbtn.setOnClickListener(new View.OnClickListener() {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Home.class);
-                startActivity(i);
-
+                validate(usernametxt.getText().toString(), passwordtxt.getText().toString());
             }
+
+
+            private void validate(String username, String password) {
+                if ((username.equals("user")) && (password.equals("1234"))) {
+                    Intent intent = new Intent(LogIn.this, Home.class);
+                    startActivity(intent);
+                } else if (isEmpty((passwordtxt.getText().toString())))
+                    Toast.makeText(getApplicationContext(), "Please enter your password", Toast.LENGTH_LONG).show();
+                    counter--;
+
+
+                    if (counter == 0) {
+                        LogIn.setEnabled(false);
+
+                    }
+                }
+
 
 
         });
@@ -43,9 +68,12 @@ public class LogIn extends AppCompatActivity {
 
     }
 
-    public void addDetasils(View v){
+    private static void setEnabled(boolean b) {
+    }
 
-        Intent click =new Intent(this, Adddetails1.class);
+    public void addDetasils(View v) {
+
+        Intent click = new Intent(this, Adddetails1.class);
         startActivity(click);
     }
 }
